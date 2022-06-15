8fe980f1e215d0dabe6d3cfbf572ad3c0eeb74fc
RashVV <johndoe@example.com>
2022-06-15 23:49:27 +0300

    edit modal for user score edit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../components/Modal.css';
 
-export default function Modal({active, setActive}) {
+export default function Modal({ active, setActive, children }) {
   return (
-    <div className={ active ? 'modal.active' : 'modal' }>
-    <div><button className='modal_close' onClick={() =>setActive(false)}>X</button></div>
-        <div className='modal_header'>1</div>
-        <div className='modal_content'>11</div>
+    <div className={ active ? 'modal_active' : 'modal' }>
+    <button className='modal_close' onClick={ () => setActive(false) }>X</button>
+        <div className='modal_content'>{ children }</div>
     </div>
   )
 }
--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -12,38 +12,50 @@ export default function ProfilesList( {fetchUsers, sortedUsers}) {
 	}, [fetchUsers])
 
   return (
-    <><ol id='profile'>
-      {item(sortedUsers, setModalActive)}
-	  <Modal active={modalActive} setActive={setModalActive} />
+    <>
+
+	<ol id='profile'>
+      {item(sortedUsers, modalActive, setModalActive)}
+
     </ol>
     </>
   )
 }
 
-function item(sortedUsers, setModalActive) {
+function item(sortedUsers, modalActive, setModalActive) {
 	return (
 		<>
 		{
 			sortedUsers?.map((value,index) => (
-				<li className='flex' key={index}>
-			<div className='item'>
-				<img src={UserImg} alt='logo' className='images' />
-				 <div className='item'>
-					<span>{value.score}</span>
-				 </div>
-				<div>
-					<h3 className='name'>{value.name}</h3>
-				</div>
-				<div className='place'>
-					<div>Place</div>
-				</div>
-				<div>
-					<button onClick={() => setModalActive(true)}>
-						<FaPen/>
-					</button>
-				</div>
-			</div>
-		</li>
+				<>
+				<Modal active={modalActive} setActive={setModalActive} key={index}>
+						<form action=''>
+							<legend className='modal_header'> Edit user score </legend>
+							<legend className='modal_nameUser'>{value.name}</legend>
+							<input className='modal_score' type='number' />
+							<br/>
+							<input type="submit" value="Save"></input>
+						</form>
+					</Modal>
+					<li className='flex' key={index}>
+					<div className='item'>
+						<img src={UserImg} alt='logo' className='images' />
+						<div className='item'>
+							<span>{value.score}</span>
+						</div>
+						<div>
+							<h3 className='name'>{value.name}</h3>
+						</div>
+						<div className='place'>
+							<div>Place</div>
+						</div>
+						<div>
+							<button onClick={() => setModalActive(true)}>
+								<FaPen />
+							</button>
+						</div>
+					</div>
+				</li></>
 		))
 		}
 		</>
