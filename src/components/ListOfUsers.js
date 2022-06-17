9d88902c395ad345efc4489cc9ffb5471a4dd5f1
RashVV <vadim.ratushnyak@gmail.com>
2022-06-17 13:05:12 +0300

    fix some bugs

--- src/components/ListOfUsers.js
+++ src/components/ListOfUsers.js
@@ -7,29 +7,29 @@ import { fetchUsersAction } from '../redux/actions';
 
 export default function ListOfUsers() {
 	const dispatch = useDispatch();
-	const oldUsers = useSelector((state) => state.usersHistoryScoreReducer);
+	const usersFromApi = useSelector((state) => state.usersHistoryScoreReducer)
+	const oldUsers = Array.from(usersFromApi);
 	const [users, setUsers] = useState(oldUsers)
 
 	useEffect(() => {
 		dispatch(fetchUsersAction());
 	  }, [dispatch]);
+	  console.log(users);
+	  const sortedUsers = oldUsers.sort((a, b) => b?.score - a?.score);
 
-
-
-	// const sortedUsers = users.sort((a, b) => b?.score - a?.score);
 
   return (
     <div className='list'>
 			<div className='list_header'>
       <h2>Leaders table for this period</h2>
 			<div className='btn'>
-				<button className='btn_Nav' onClick={ setUsers }>{'<<'}</button>
+				<button className='btn_Nav' onClick={ setUsers(sortedUsers) }>{'<<'}</button>
 				<button className='btn_Nav' disabled >{'>>'}</button>
-				<button className='btn_newDay' onClick={fetchUsersAction}>new day</button>
+				<button className='btn_newDay' onClick={fetchUsersAction()}>new day</button>
 				<button className='btn_addNewUser'>+ Add new user</button>
 			</div>
 			</div>
-			<ProfilesList /*sortedUsers={sortedUsers}*/ fetchUsers={fetchUsersAction}></ProfilesList>
+			<ProfilesList sortedUsers={sortedUsers} fetchUsers={fetchUsersAction}></ProfilesList>
     </div>
   )
 }
