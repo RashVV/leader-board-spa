f59fd21f97fe9b6392d707a6ecb71bbdc2caf820
RashVV <johndoe@example.com>
2022-07-04 22:15:09 +0300

    bring fetch out

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,12 +1,12 @@
 import { EDITED_USER_SCORE, ADDED_NEW_USER, FETCHED_USERS, FETCH_FAILED, FETCHED_NEW_USERS, PAGINATED_USERS } from './actionType';
-import axios from 'axios';
-import getUsers from '../services/fetchUsers';
+import {getUsers} from '../services/fetchUsers';
+
 
 export const fetchUsersAction = () => {
 	return async (dispatch) => {
 		try {
       debugger
-      const places =  getUsers();
+      const places = await getUsers();
 		    dispatch({ type: FETCHED_USERS, payload: places });
 	  } catch (e) {
 		  console.warn(e)
@@ -18,12 +18,7 @@ export const fetchUsersAction = () => {
 export const fetchNewUsersAction = () => {
 	return async (dispatch) => {
 		try {
-			let response = await axios.get('http://coding-test.cube19.io/frontend/v1/starting-state');
-			const payload = response.data.map((value) => value.score ? value : { ...value, score:0 }).sort((a, b) => b?.score - a?.score)
-      const places = payload.map((_, index) => ({
-        ..._,
-        place: index+1
-      }))
+        const places = await  getUsers()
 		    dispatch({ type: FETCHED_NEW_USERS, payload: places });
 	  } catch (e) {
 		  console.warn(e)
--- src/services/fetchUsers.js
+++ src/services/fetchUsers.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-export default async  function getUsers() {
-  debugger
+export  async  function getUsers() {
   const url = 'http://coding-test.cube19.io/frontend/v1/starting-state';
-   await axios.get(`${url}`)
-      .then((response) => {
-        const payload = response.data.map((value) => value.score ? value : { ...value, score:0 }).sort((a, b) => b?.score - a?.score);
+  try{
+    const response =  await axios.get(`${url}`)
+    const payload = response.data.map((value) => value.score ? value : { ...value, score:0 }).sort((a, b) => b?.score - a?.score);
         const places = payload.map((_, index) => ({
-        ..._,
-        place: index+1
-      }))
-      return  [ places ]
-    })
-    .catch(error => console.error(`Error: ${error}`));
+          ..._,
+          place: index+1
+        }))
+      debugger
+      return places
+
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 
